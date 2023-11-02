581a567e8d8077c6a15d7832efa160a03032a40b
andrescuello7 <andrescuellotrabajo@gmail.com>
2023-11-02 02:52:18 -0300

    update api ppost and comment relation OneToMany

--- src/main/java/com/example/apipostgress/models/posts/PostModel.java
+++ src/main/java/com/example/apipostgress/models/posts/PostModel.java
@@ -10,9 +10,13 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+import com.example.apipostgress.models.users.UserModel;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
@@ -32,6 +36,12 @@ public class PostModel implements Serializable {
 
   @Column(name = "photo", columnDefinition = "TEXT DEFAULT ''")
   private String photo;
+
+
+  @JsonBackReference
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "user_id")
+  private UserModel user;
 
   @JsonManagedReference
   @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
